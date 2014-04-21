17db4e617a4b79520dccf508727ab437ccabab10
bcwaters <bcwaters@asu.edu>
2014-04-20 18:00:43 -0700

    Update Driver.java
    Removed comments from new lines of code

--- Source Code 2/Driver.java
+++ Source Code 2/Driver.java
@@ -1,9 +1,9 @@
 package create.gui;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.util.Scanner;
-//import java.util.*;
-//import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
+import java.io.PrintWriter;
 
 
 public class Driver 
@@ -13,11 +13,11 @@ public class Driver
 	public static void main(String[] args)
 	{
 		SignInFrame frame = new SignInFrame();
-		//Database database = new DataBase();
+		Database database = new DataBase();
 		frame.start();
-		//frame.setDatabase();
-		//have the sign in frame hold a database.  
-		//All data is now available via signinframe
+		frame.setDatabase();
+		have the sign in frame hold a database.  
+		All data is now available via signinframe
 	}
 	
 }
