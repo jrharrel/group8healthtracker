3c2835d69c8866f359035196bb1f52349ae05683
bcwaters <bcwaters@asu.edu>
2014-04-20 17:36:08 -0700

    Update Driver.java
    added in commented code

--- Source Code 2/Driver.java
+++ Source Code 2/Driver.java
@@ -1,12 +1,23 @@
 package create.gui;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.Scanner;
+//import java.util.*;
+//import java.io.PrintWriter;
+
 
 public class Driver 
 {
+	
+	
 	public static void main(String[] args)
 	{
 		SignInFrame frame = new SignInFrame();
+		//Database database = new DataBase();
 		frame.start();
-		
+		//frame.setDatabase();
+		//have the sign in frame hold a database.  
+		//All data is now available via signinframe
 	}
 	
 }
